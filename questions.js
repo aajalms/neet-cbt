dc125a4053bc46032295e8740b8407fdddbdda6e
aajalms <aajalms@gmail.com>
2026-02-19 13:19:01 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    q: "Normal pacemaker of heart is?",
+    options: ["AV node","SA node","Bundle of His","Purkinje fibres"],
+    correct: 1
+  },
+  {
+    q: "Functional unit of kidney is?",
+    options: ["Neuron","Nephron","Alveolus","Lobule"],
+    correct: 1
+  },
+  {
+    q: "Vitamin C deficiency causes?",
+    options: ["Rickets","Scurvy","Beriberi","Pellagra"],
+    correct: 1
+  }
+];
