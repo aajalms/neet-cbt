21e2fa183b129b27ff26875f3de050bdf076eeaf
aajalms <aajalms@gmail.com>
2026-02-22 22:29:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.API_URL = "https://script.google.com/macros/s/AKfycbyZugv4yvXHzGgaIu3MZdDHMpijrH2h1rbNkZB2M1kiGmdIBQMnVTbDqi435iqdNC_-/exec";
+window.EXAM_DURATION_MIN = 60;
+window.MARKS_CORRECT = 4;
+window.MARKS_WRONG = -1;
