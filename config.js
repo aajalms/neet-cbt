56dc059e8e1d9a2d5aadecfa6a6a0d0572a8a5b4
aajalms <aajalms@gmail.com>
2026-02-22 22:53:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.API_URL = "https://script.google.com/macros/s/AKfycbyZugv4yvXHzGgaIu3MZdDHMpijrH2h1rbNkZB2M1kiGmdIBQMnVTbDqi435iqdNC_-/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbyrR6uJDuFZBkbrtbjiTuIrKB31alvPWVmz4znmccpqPR_u1TLIqxYfziq9QJObdWBL/exec";
 window.EXAM_DURATION_MIN = 60;
 window.MARKS_CORRECT = 4;
 window.MARKS_WRONG = -1;
